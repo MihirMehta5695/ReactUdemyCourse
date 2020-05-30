e986cfeb4980e9022b773015e4b7b0cba34828d4
Mihir <mihirm5695@gmail.com>
2020-05-30 17:11:14 +0530

    Let_Const_VAR examples with hoisting

--- RefresherES6/let_const.js
+++ RefresherES6/let_const.js
@@ -1,17 +1,22 @@
 var myName = "Mihir";
 
-console.log(myName);
-
+// console.log(myName);
+console.log(myNumber); // Example of hoisting
 const Person = function (name, age, profession) {
+    var personPrint = "Name : " + name + "\nAge : " + age + "\nProfession : " + profession;
     personName = name;
     const personAge = age;
-    let personProfession = age;
+    // Cannot perform the reassign operation on const.
+    /* personAge = name */
+    let personProfession = age; /* Can be reassigned */
     personProfession = profession;
-    console.log(name, age, profession);
-    console.log(personName, personAge, personProfession);
+    // console.log(name, age, profession);
+    // console.log(personName, personAge, personProfession);
+    console.log(personPrint)
 }
 
 Person("Mihir", 24, "Developer");
-console.log(personName);
-console.log(personAge);
-console.log(personProfession);
+console.log(personName); // This will be printed
+/* console.log(personAge); */ // This wont work because of the variable scoping
+/* console.log(personProfession); */ // This also wont work because of the variable scoping
+var myNumber = 24;
