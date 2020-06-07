639d3c435c232ef534018c135c6fc13e223c728b
Mihir <mihirm5695@gmail.com>
2020-06-07 18:26:43 +0530

    Learned -> Understanding & Using Props and States -> 40

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,23 +5,39 @@ import Person from './Person/Person.js'
 // But component name should always start with capital, as it will be used as tag -> Person, Animal
 // In react, the tags starting from smaller case are considered as tags -. eg: div, p, br, hr, table, etc 
 class App extends Component {
-  render() {
-    // className is restriction of JSX as class is a reserved keyword in JS
-    return (
-      // This is similar to HTML but it is actually JSX
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <Person name="Mansi" age="19" />
-        <Person name="Mihir" age="25">My hobbies : Watching Movies!</Person>
-      </div>
-      // <h1> Another Heading</h1> ->
-      // As JSX can have only one root element
-    );
-    // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!');
-    // return React.createElement('div', null, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
 
-  }
+    // only works when we have extended React.Component
+    // props is data sent from another component
+    // state is data maintained internally by the component
+    // state is a reserved word
+    state = {
+        persons: [
+            { name: 'Mihir', age: 25 },
+            { name: "Mansi", age: 19 },
+            { name: "Bhavesh", age: 53 }
+        ]
+    };
+
+    render() {
+        // className is restriction of JSX as class is a reserved keyword in JS
+        return (
+            // This is similar to HTML but it is actually JSX
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies : Watching Movies!</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+            // <h1> Another Heading</h1> ->
+            // As JSX can have only one root element
+        );
+        // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!');
+        // return React.createElement('div', null, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+        // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+
+    }
 }
 
 export default App;
