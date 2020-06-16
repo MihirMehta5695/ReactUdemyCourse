08603950c3db87b91f48605cf23353ca7eb8a0d1
Mihir <mihirm5695@gmail.com>
2020-06-16 15:54:06 +0530

    Learned -> Flexible Lists -> 61

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -26,17 +26,22 @@ class App extends Component {
 
     // This will be passed down to the child element,
     // and will be called whenever there is a change in the input type text of person
-    nameChangedHandler = event => {
+    nameChangedHandler = (event, id) => {
 
-        // event contains the target on which the event was triggered
-        // and target is input type text, thus it also has the attribute value
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const person = { ...this.state.persons[personIndex] }
+
+        const personAlternative = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
 
         this.setState({
-            persons: [
-                { name: "Mihir", age: 25 },
-                { name: event.target.value, age: 19 },
-                { name: "Bhavesh", age: 53 }
-            ]
+            persons: persons
         });
     }
 
@@ -99,6 +104,7 @@ class App extends Component {
                                     name={person.name}
                                     age={person.age}
                                     key={person.id}
+                                    myChange={(event) => this.nameChangedHandler(event, person.id)}
                                 />
                             )
                         })}
