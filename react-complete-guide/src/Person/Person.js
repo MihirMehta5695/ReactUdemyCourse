28095503917ceaf969658e28922b97718d96270c
Mihir <mihirm5695@gmail.com>
2020-06-21 00:10:35 +0530

    Learned -> CSS Modules & Media Queries -> 75

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -5,8 +5,7 @@
 // Also called as dumb component or presentational component
 
 import React from 'react';
-// import './Person.css';
-import styled from 'styled-components'
+import classes from './Person.css';
 // Remember to import the css file to apply css styles
 
 // This is possible
@@ -24,29 +23,16 @@ import styled from 'styled-components'
 // props can have any  name
 // When using class-based components, we access props by -> this.props
 
-const StyledDiv = styled.div`        
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`
-
 const person = (props) => {
     // 'children' is a reserved keyword
     // we can access the data passed between the component tag using props.children
     return (
         // <div className="Person" style={myStyle}>
-        <StyledDiv>
+        <div className={classes.Person}>
             < p onClick={props.myClick} > I'm a {props.name} and I am {props.age} years old!</p>
             < p > {props.children}</p >
             <input type="text" onChange={props.myChange} value={props.name} />
-        </StyledDiv>
+        </div>
     )
     // here, the props.myClick is the function passed by the parent component
     // here, the props.myChange is the function passed by the parent component
