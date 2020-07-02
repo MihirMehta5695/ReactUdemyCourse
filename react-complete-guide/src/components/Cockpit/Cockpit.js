6a90f229e3083948f2df18b3df110df9539b6089
Mihir <mihirm5695@gmail.com>
2020-07-02 16:52:40 +0530

    Learned -> Using useEffect() in Functional Components -> 94

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css'
 
+// after useState, the next import react-hook you can use is the useEffect() hook.
+// useEffect combines all the life-cycle hooks in class-based components into one method called useEffect() for functional components.
 const cockpit = props => {
+
+    // useEffect is called for every render cycle
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // http requests
+    });
 
     // let classes = ['red', 'bold'].join(' ');//returns "red bold"
     const assignedClasses = [];
