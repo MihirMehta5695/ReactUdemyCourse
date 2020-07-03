c9d482e914c18b6700130af6991320dd2fd90f61
Mihir <mihirm5695@gmail.com>
2020-07-03 17:20:06 +0530

    Learned -> PureComponents instead of shouldComponentUpdate -> 101

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person'
 
-export default class Persons extends Component {
+// PureComponents are used when we need to check all elemnts of props in shouldComponentUpdate. 
+// We can skip the implementation of shouldComponentUpdate with the use of PureComponent
+/*
+For EG:
+   shouldComponentUpdate(nextProps, nextState) {
+        if (
+            nextProps.persons !== this.props.persons ||
+            nextProps.showPersons !== this.props.showPersons ||
+            nextProps.changed !== this.props.changed
+            ) {
+            return true;
+        } else {
+            return false;
+        }
+        // return false; //prevents update
+    } 
+*/
+export default class Persons extends PureComponent {
 
     // Only works if there is state initialized
     /*static getDerivedStateFromProps(props, state) {
@@ -21,10 +38,17 @@ export default class Persons extends Component {
 
         }
     */
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[Persons.js] shouldComponentUpdate')
-        return true;
-    }
+    //    If parent is going to update and pass new props, makes no sense of implementing shouldComponentUpdate()
+/*     shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        if (nextProps.persons !== this.props.persons) {
+            return true;
+        } else {
+            return false;
+        }
+        // return false; //prevents update
+    } */
+
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log('[Persons.js] getSnapshotBeforeUpdate')
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -51,12 +51,7 @@ class App extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[App.js] shouldComponentUpdate');
-        // if (nextProps.persons !== this.props.persons) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        return true; //prevents update
+        return true;
     }
 
     componentDidUpdate() {
