96594bc7ae98a86abf49db99e880271ace7f30bb
Mihir <mihirm5695@gmail.com>
2020-07-04 18:16:05 +0530

    Learned -> Rendering Adjacente JSX Elements -> 103,104

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../containers/hoc/Auxiliary/Auxiliary';
 class Person extends Component {
 
     render() {
-
-        return (
+        // This way works as this is the traditional way of rendering elements, where we have a root element.
+        /*return (
             <div className={classes.Person} >
                 <p
                     onClick={this.props.myClick} >
@@ -16,6 +17,38 @@ class Person extends Component {
                     onChange={this.props.myChange}
                     value={this.props.name} />
             </div>
+        )*/
+        // It is true that adjacent jsx elements cannot be rendered and all of them must ve enclosed in a root element.
+        //But the other way to render adjacent elements
+        // Returning the array of elements can make rendering adjacent jsx elements possible.
+        // remember to assign unique key to each of the member of the array.
+        /*return [
+            <p
+                key="i1"
+                onClick={this.props.myClick} >
+                I'm a {this.props.name} and I am {this.props.age} years old!
+            </p>,
+            <p key="i2"> {this.props.children}</p>,
+            <input
+                key="i3"
+                type="text"
+                onChange={this.props.myChange}
+                value={this.props.name} />
+        ];*/
+
+        // In order to escape the array part and div part, we can create our own Auxillary components.
+        return (
+            <Aux>
+                <p
+                    onClick={this.props.myClick} >
+                    I'm a {this.props.name} and I am {this.props.age} years old!
+                </p>
+                <p> {this.props.children}</p>
+                <input
+                    type="text"
+                    onChange={this.props.myChange}
+                    value={this.props.name} />
+            </Aux>
         )
     }
 }
