14586a85f6ff42f51e99725aa7c31dd52f7ea2eb
Mihir <mihirm5695@gmail.com>
2020-07-05 13:10:31 +0530

    Learned -> Using Refs -> 111

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -5,17 +5,35 @@ import Aux from '../../../containers/hoc/Auxiliary/Auxiliary';
 import withJSClass from '../../../containers/hoc/With/withJSClass';
 class Person extends Component {
 
+
+    constructor(props) {
+        super(props)
+        // This work only in react v16+
+        this.elementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // This focuses on the first input element it finds globally
+        // document.querySelector('input').focus();
+
+        this.inputElement.focus(); //This works in all versions of react
+        this.elementRef.current.style.color = 'red';  // This work only in react v16+
+    }
+
     render() {
         // React has its own Auxillary COmponent called as the Fragment component
         return (
             <Aux>
                 <p
+                    ref={this.elementRef}
                     onClick={this.props.myClick} >
                     I'm a {this.props.name} and I am {this.props.age} years old!
                 </p>
                 <p> {this.props.children}</p>
+                {/*the ref works in all versions of react*/}
                 <input
                     type="text"
+                    ref={(inputEl) => { this.inputElement = inputEl }}
                     onChange={this.props.myChange}
                     value={this.props.name} />
             </Aux>
