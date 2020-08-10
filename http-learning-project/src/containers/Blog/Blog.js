89d5e6818d40025a381e6d7d4750d6472dada68c
Mihir <mihirm5695@gmail.com>
2020-08-10 17:20:06 +0530

    173. Setting a Default Global Configuration for Axios

--- http-learning-project/src/containers/Blog/Blog.js
+++ http-learning-project/src/containers/Blog/Blog.js
@@ -16,7 +16,7 @@ class Blog extends Component {
     componentDidMount() {
         // wrong error URL for testing
         // axios.get('https://jsonplaceholder.typicode.com/postsssss')
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
