048af19313186364113ae8fa061696580d6b49f1
Mihir <mihirm5695@gmail.com>
2020-08-11 18:48:02 +0530

    174. Creating and Using Axios Instances

---
+++ http-learning-project/src/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// You can create multiple copy of axios instance!
+// But this should be done in different files.
+// This instance overrides settings of the global axios instance {declared in @index.js}.
+const instance = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com"
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+export default instance;
