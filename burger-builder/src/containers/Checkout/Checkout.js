7125477c27e16689ec1c6e1e246131febd1211ea
MihirMehta5695 <mihirm5695@gmail.com>
2020-08-30 18:28:14 +0530

    224. Navigating Back & To Next Page

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -10,13 +10,25 @@ export default class Checkout extends Component {
             meat: 1,
             bacon: 1
         }
+
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
     }
 
     render() {
 
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutContinued={this.checkoutContinuedHandler}
+                    checkoutCancelled={this.checkoutCancelledHandler} />
             </div>
         )
 
