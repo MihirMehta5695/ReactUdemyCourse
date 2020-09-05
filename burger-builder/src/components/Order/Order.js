d930b0e20f7adfd3d0051e360f42b8791565af76
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-05 23:32:17 +0530

    229. Implementing Navigation Links

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,5 +1,5 @@
-import { React } from 'react';
-import classes from '*.module.css';
+import React from 'react';
+import classes from './Order.module.css';
 
 const order = (props) => (
     <div className={classes.Order}>
--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -11,6 +11,7 @@ class Orders extends Component {
         return (
             <div>
                 <Order />
+                <Order />
             </div>
         );
     }
