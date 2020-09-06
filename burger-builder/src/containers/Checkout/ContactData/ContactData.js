b55cc5af30c6d2bd4442bedadf80e133c6e720dd
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-06 19:36:47 +0530

    240. Handling User Input

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -63,6 +63,19 @@ export default class ContactData extends Component {
 
         loading: false
     }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = { ...updatedOrderForm[inputIdentifier] }
+
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm })
+    };
+
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({ loading: true })
@@ -97,7 +110,8 @@ export default class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value} />
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
