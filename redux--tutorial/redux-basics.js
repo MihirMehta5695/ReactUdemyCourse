fdef73a5b489a972420e43f2fc755bdd92e3c96a
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-08 11:25:40 +0530

    256. Dispatching Actions

--- redux--tutorial/redux-basics.js
+++ redux--tutorial/redux-basics.js
@@ -10,6 +10,21 @@ const intialState = {
 // Reducer
 const rootReducer = (state = intialState, action) => {
     // This is a valid and simplest reducer which returns the same old state.
+    // return state;
+    if (action.type === "INC_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === "ADD_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+
     return state;
 };
 
@@ -18,5 +33,15 @@ const store = createStore(rootReducer);
 console.log(store.getState())
 
 // Dispatching Action
+// TO dispatch an action we will use our store
+// The dispatch() take a javascript object as an argument
+// This argument is called an action.
+// The object must have a key called "type" and it can store any value of your choice.
+store.dispatch({ type: "INC_COUNTER" });
+// INC_COUNTER add 1 to the counter, so no payload is required
+// ADD_COUNTER lets you add any number to the current state of counter, so here a payload is required to pass on the number by which the counter should be incremented.
+// store.dispatch({ type: "ADD_COUNTER", payload: {value:10} });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState())
 
 // Subscription
