46abbcd506fc5cd10fdb098b0a247a5a042f5449
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-11 18:44:43 +0530

    267. Combining Multiple Reducers

--- redux--tutorial/src/index.js
+++ redux--tutorial/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import { createStore, combineReducers } from 'redux';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
 
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+});
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 // Provider is a helper component which helps us to inject redux store into our react application.
 // FInally, to hook up our React project with Redux, we need to pass on a special property to the Provider
 
--- redux--tutorial/src/store/reducers/counter.js
+++ redux--tutorial/src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions'
+import * as actionTypes from '../actions'
 
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 };
 
 const reducer = (state = initialState, action) => {
@@ -30,23 +29,6 @@ const reducer = (state = initialState, action) => {
                 counter: state.counter - action.value
             };
 
-        case actionTypes.STORE_RESULT:
-            // Array.concat add element in immuttably
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter })
-            }
-
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-
-            const updatedArray = state.results.filter(result => result.id !== action.resultElementId)
-            return {
-                ...state,
-                results: updatedArray
-            }
     }
 
     return state;
