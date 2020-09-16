4d114510d308c381f2ea98f6d05c81f6f599621a
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-16 18:23:42 +0530

    279. Redux & UI State

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,6 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -39,7 +38,7 @@ class BurgerBuilder extends Component {
         const sum = Object.keys(ingredients).map(igKey => ingredients[igKey])
             .reduce((sum, el) => (sum + el), 0);
 
-        this.setState({ purchasable: sum > 0 });
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -88,7 +87,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </Aux>
