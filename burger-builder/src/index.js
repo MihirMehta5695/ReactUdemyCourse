9cab597128e318d34f8eb03b38af6b88a3c076f1
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-16 11:26:23 +0530

    275. Basic Redux Setup

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -12,11 +12,9 @@ const store = createStore(reducer);
 
 const app = (
   <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
