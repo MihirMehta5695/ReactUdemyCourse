67d4e64374d9370d7780e3c4f89dcf5e6252bb2f
MihirMehta5695 <mihirm5695@gmail.com>
2020-09-17 16:45:44 +0530

    284. Adding Middleware2~

--- redux--tutorial/src/index.js
+++ redux--tutorial/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
@@ -13,9 +13,23 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+// custom Middleware
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action)
+            const result = next(action)
+            console.log('[Middleware] next state', store.getState())
+            return result;
+        }
+    }
+};
+
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 // Provider is a helper component which helps us to inject redux store into our react application.
 // FInally, to hook up our React project with Redux, we need to pass on a special property to the Provider
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
